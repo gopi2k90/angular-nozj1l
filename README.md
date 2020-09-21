bdc311c575a56653c5f051445fe4c31e49927591
gopi2k90 <gopi2k90@gmail.com>
2020-09-21 12:23:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nozj1l
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nozj1l)
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { InsSheetComponent } from './ins-sheet/ins-sheet.component';
+import { ComponentsModule } from './components/components.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    ComponentsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    InsSheetComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
